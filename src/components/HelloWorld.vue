626d173a90d7a9d384c6cbf76112b8a822624e6d
liaowentuan <996002654@qq.com>
2018-08-01 10:00:02 +0800

    background

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="hello">
+    <div class="background">
+
+    </div>
 
   </div>
 </template>
@@ -15,5 +18,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.hello{
+  .background{
+    background: url('/static/img/3bad1ed4ce5beaab815d8fbd97729e46.jpg') no-repeat 0 center fixed;
+    background-size: 100%;//此处要注意，可以填cover也可以填100%。建议填100%
+    display: block;
+    min-height: 400px;
+    position: fixed;
+    left: 0;
+    top: 40px;
+    right: 0;
+  }
+}
 </style>
