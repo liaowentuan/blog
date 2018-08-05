76cbcee3f5a2727448e7422f88c51be77e1580c4
liaowentuan <996002654@qq.com>
2018-08-05 22:27:41 +0800

    common

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,10 +1,12 @@
 const mutations = {
+  // 选择语言start
   chooseEn: state => {
     state.Lang = 'en'
   },
   chooseZh: state => {
     state.Lang = 'zh-cn'
   }
+  // 选择语言end
 }
 
 export default mutations
